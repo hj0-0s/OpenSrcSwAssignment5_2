362204fe60effb7a2d91fb308c6726f6992a3376
hj0-0s <jaesang00@konkuk.ac.kr>
2020-04-25 18:38:51 +0900

    Gugudan ver.1.5 update

--- Gugudan.java
+++ Gugudan.java
@@ -5,8 +5,15 @@ public class Gugudan {
         Scanner scan = new Scanner(System.in);
         System.out.print("출력시킬 단 : ");
         int num = scan.nextInt();
-        for (int i = 1; i <= 9; i++)
-            System.out.println(num + " * " + i + " = " + (num * i));
-
+        if (num == 0) {
+            for (int i = 1; i <= 9; i++) {
+                for (num = 2; num <= 9; num++)
+                    System.out.printf("%d * %d = %2d  ", num, i, (num * i));
+                System.out.println();
+            }
+        } else {
+            for (int i = 1; i <= 9; i++)
+                System.out.println(num + " * " + i + " = " + (num * i));
+        }
     }
 }
